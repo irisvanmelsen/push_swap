a02ebbbd047141c2edca3d67e118bc1d8ff9ea10
Iris Van Melsen <ivan-mel@f0r5s18.codam.nl>
2023-02-06 18:15:20 +0100

    fixed everything except working algo

--- src/quicksort.c
+++ src/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: ivan-mel <ivan-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 13:57:00 by ivan-mel          #+#    #+#             */
-/*   Updated: 2023/02/06 14:33:04 by ivan-mel         ###   ########.fr       */
+/*   Updated: 2023/02/06 17:46:06 by ivan-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,18 @@ void	quicksort(t_stack **stack_a, t_stack **stack_b)
 	algorithm(stack_a, stack_b, find_pivot(*stack_a));
 }
 
+// pb: take the first element at the top of a and put it at the top of b.
+// ra: shift up all elements of stack a by 1, the first element
+// becomes the last one.
+
 void	algorithm(t_stack **stack_a, t_stack **stack_b, int pivot)
 {
-	t_stack	*last;
 	t_stack	*tmp;
 
-	last = *stack_a;
 	tmp = *stack_a;
-	while (last->next != tmp)
-		last = last->next;
-	while (tmp != last)
-	{
-		if (tmp->nb < pivot)
-		{
-			pb(stack_a, stack_b);
-		}
-		tmp = tmp->next;
-	}
+	if (tmp->nb < pivot)
+		pb(stack_a, stack_b);
+	else
+		ra(stack_a);
+	tmp = tmp->next;
 }
